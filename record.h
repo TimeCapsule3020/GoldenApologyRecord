e1ab47e27a0846a0c2786427ceae18c38881c97e
TimeCapsule3020 <60403580+TimeCapsule3020@users.noreply.github.com>
2020-01-29 01:33:55 +0100

    Create record.h

---
+++ record.h
@@ -0,0 +1,40 @@
+#ifndef RECORD_H
+#define RECORD_H
+ 
+/*
+ 
+Liebe Nachfahren,
+ 
+ich habe diese Zeilen aufgeschrieben, in der Hoffnung in 1000 Jahren gefunden und gelesen zu werden.
+1000 Jahre ist der von Wissenschaftlern geschätzte Zeitraum, in dem das bisher im 20. und 21. Jahrhundert von unserer Gesellschaft ausgestossene Kohlendioxid unverändert seine Wirkung als sogenanntes Treibhausgas entfalten kann und nicht merklich abnehmen wird.
+Die gravierten Glasplatten in diesem ehemaligen Bergwerkstollen überdauern hoffentlich unbeschadet die kommenden Umbrüche und das Chaos, das die industrialisierte Menschheit (und damit auch ich) durch die massive Beeinflussung des globalen Klimas und der Natur ausgelöst hat.
+Dieser Text hat eine einzige Aussage als Ziel: Es tut mir leid, dass ich nicht genügend dagegen unternommen habe, gegen die Zerstörung der Natur und gegen den Hass und die Lieblosigkeit anderen Menschen gegenüber.
+
+In eine Wohlstandsgesellschaft hineingeboren, habe ich in den ersten Jahrzehnten meines Lebens, davon bereits zwei Jahrzehnte berufstätig, vorrangig eigene Interessen verfolgt und unbekümmert den Wohlstand mit seinen Heilsversprechen genossen. Obwohl in meiner Schulzeit das Wissen um die zerstörerischen Einflüsse des Menschen auf die Natur und auf sich selber vermittelt wurde, blieb ich trotz dieses Bewußtseins seltsam untätig.
+Diese Untätigkeit stammt zu einem geringen Teil von tatsächlichen Vorgaben und Einschränkungen (den sog. Sachwängen, zum Beispiel Steuern oder auch Gesetze), mit denen man in der bestehenden Gesellschaft konfrontiert ist und meist auch wieder davon profitiert. Ein etwas größerer Teil rührt von der übernommenen Verantwortung für die eigene Familie und das eigene Wohlergehen (Vorsorge für Gesundheit und Alter) und sich daraus ergebende weitere Sachzwänge. Der größte Teil aber ist unbegründet und stammt schlicht und einfach von der eigenen Bequemlichkeit, gepaart mit Zaghaftigkeit und Verdrängung.
+ 
+Dabei liegt im gerade begonnenen 21. Jahrhundert vieles im Argen:
+ 
+- Rücksichtsloses Wirtschaftssystem mit systematischer Ausbeutung vieler Menschen in benachteiligten Ländern
+- Zerstörung von Lebensgrundlagen, der gesamten Umwelt und letztendlich der Biosphäre (bewusst in Kauf genommene Veränderung des globalen Klimas, Verschmutzung und Vergiftung von Erde, Luft und aller Gewässer, Zerstörung von Biotopen und Lebensräumen) zugunsten kurzfristiger Vorteile
+- Ausbeutung, Manipulation und Auslöschen/Verdrängen anderer Lebewesen
+- bisher kein sicheres Verwahren von radioaktiven Hinterlassenschaften mit dem Risiko schwerster gesundheitlicher und ökologischer Schäden
+- Fortwährender Rassismus, Intoleranz und Ausgrenzung anderer Bevölkerungsgruppen, Ethnien und Kulturen, Lebensweisen und Weltanschauungen
+- Erosion der Nächstenliebe und Verlust des Altruismus in großen Teilen der Gesellschaft, einhergehend mit erhöhter Gewaltbereitschaft und Hass. Die verheerenden Kriege und Gräueltaten des 20. Jahrhunderts scheinen vergessen und drohen sich erneut zu wiederholen.
+- Die Wohlstandsgesellschaft grenzt sich immer mehr gegenüber Benachteiligten aus anderen Ländern ab. Viele Staaten in Europa und Nordamerika und andere Länder wie Australien schotten sich mittlerweile rigoros ab. Ankommende Flüchtlinge werden wie gefährliche Staatsfeinde betrachtet und behandelt. An vielen Grenzen findet man hohe
+Zäune aus Beton und Stacheldraht, überwacht von Tausenden schwerbewaffneten Grenzpolizisten und immer im Blick der Überwachungskameras. Zum Beispiel das Mittelmeer als natürliche Grenze versuchen viele Menschen in einfachen Booten zu
+überqueren und werden von den zuständigen Staaten aber oft in Stich gelassen. Tausende Menschen sind so in den letzten Jahrzehnten verschollen und ertrunken und Europa schaut weg. Statt nach Lösungen zu suchen, wie man diese Menschen angesichts der eigenen priveligierten Position dabei unterstützen kann, ihr Glück zu finden, streiten die Staaten über die Rechtmäßigkeit deren Rettung und wer sich um die Ankömmlinge kümmern muss. Kaum im Paradies angekommen, erfahren viele Menschen erneute Ablehnung, werden zum Vegetieren in Lager weggesperrt, in der Landwirtschaft wie Sklaven ausgebeutet oder müssen ständig mit ihrer Abschiebung in die Herkunfsländer rechnen.
+
+Ein Hoffnungsschimmer sind die jungen Menschen unserer Gesellschaft, die bei diesen Geschehnissen nicht mehr tatenlos zusehen wollen. Viele andere versuchen ebenfalls ihren kleinen Beitrag zu einer besseren Zukunft zu leisten, und auch ich.
+
+So schreibe ich diesen Text, damit ihr erfahrt, dass es in unserer Zeit noch Menschen gegeben hat, die Euch lieben und denen Eure Zukunft nicht egal ist!
+
+Liebe Grüße und Gottes Segen
+
+Andreas Dietz
+ 
+*/
+ 
+ 
+ 
+#endif /* RECORD_H */
